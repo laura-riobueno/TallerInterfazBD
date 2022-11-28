7f1961983671c82b98e13f666e7bb3b67b1c6ee5
Davidleofp <65404234+Davidleofp@users.noreply.github.com>
2022-11-28 18:51:55 -0500

    Panel

--- TallerInterfazBD/src/Interfaz/Interfaz.java
+++ TallerInterfazBD/src/Interfaz/Interfaz.java
@@ -1,9 +1,26 @@
 package interfaz;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.TitlePaneLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Interfaz extends JFrame implements ActionListener {
 
+    public Interfaz(){
+        setTitle("Inicio de usuario");
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[]args){
+
+        Interfaz interfaz = new Interfaz();
+        interfaz.setBounds(0,0,500,700);
+        interfaz.setVisible(true);
+        interfaz.setLocationRelativeTo(null);
+
+    }
 }
