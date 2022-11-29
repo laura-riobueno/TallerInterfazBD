f49343aad5076a7d058fc28d89d6b41de503a5be
Laura Riobueno <65404334+laura-riobueno@users.noreply.github.com>
2022-11-28 22:10:30 -0500

    Interfaz

--- TallerInterfazBD/src/Interfaz/Interfaz.java
+++ TallerInterfazBD/src/Interfaz/Interfaz.java
@@ -1,100 +1,287 @@
-package Interfaz;
 
-import javax.swing.*;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.TitlePaneLayout;
-import javax.swing.text.AttributeSet.ColorAttribute;
-import javax.swing.ImageIcon;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.*;
+package interfaz;
 
-public class Interfaz extends JFrame implements ActionListener{
-    private JLabel cedula,nombre,apellido,correo,telefono;
-    private JTextField ced,nom,ape,cor,tel;
-    private JButton Limpiar,Volver,Enviar;
+import javax.swing.JTextField;
 
+public class Interfaz extends javax.swing.JFrame {
 
-    public Interfaz(){
-        
-        setTitle("Inicio de usuario");
-        setLayout(null);
-        getContentPane().setBackground(new Color(237, 187, 153));
-        cedula = new JLabel("Cedula");
-        cedula.setBounds(50,20,200,35);
-        add(cedula);
-
-        nombre = new JLabel("Nombre");
-        nombre.setBounds(50,60,200,35);
-        add(nombre);
-
-        apellido = new JLabel("Apellido");
-        apellido.setBounds(50,100,200,35);
-        add(apellido);
-
-        correo = new JLabel("Correo");
-        correo.setBounds(50,140,200,35);
-        add(correo);
-
-        telefono = new JLabel("Telefono");
-        telefono.setBounds(50,180,200,35);
-        add(telefono);
-
-        ced = new JTextField("");
-        ced.setBounds(150,20,300,35);
-        add(ced);
-
-        nom = new JTextField("");
-        nom.setBounds(150,60,300,35);
-        add(nom);
-
-        ape = new JTextField("");
-        ape.setBounds(150,100,300,35);
-        add(ape);
-
-        cor = new JTextField("");
-        cor.setBounds(150,140,300,35);
-        add(cor);
-
-        tel = new JTextField("");
-        tel.setBounds(150,180,300,35);
-        add(tel);
-
-        Limpiar = new JButton("Borrar Datos");
-        Limpiar.setBounds(150,220,150,40);
-        add(Limpiar);
-        Limpiar.addActionListener(this);
-
-        Enviar = new JButton("Enviar Datos");
-        Enviar.setBounds(300,220,150,40);
-        add(Enviar);
-        Enviar.addActionListener(this);
-
-        Volver = new JButton("Volver");
-        Volver.setBounds(150,270,150,40);
-        add(Volver);
-        Volver.addActionListener(this);
-    }
-
-    public void actionPerformed(ActionEvent L){
-        if(L.getSource()==Limpiar){
-
-            ced.setText("");
-            nom.setText("");
-            ape.setText("");
-            cor.setText("");
-            tel.setText("");
-
-        }
-    }
     /**
-     * @param args
+     * Creates new form Interfaz
      */
-    public static void main(String[]args){
-
-        Interfaz interfaz = new Interfaz();
-        interfaz.setBounds(0,0,500,500);
-        interfaz.setVisible(true);
-        interfaz.setLocationRelativeTo(null);
-
+    public Interfaz() {
+        initComponents();
     }
+
+    @SuppressWarnings("unchecked")
+    
+    private void initComponents() {
+
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtCedula = new javax.swing.JTextField();
+        txtNombre = new javax.swing.JTextField();
+        txtApellido = new javax.swing.JTextField();
+        txtCorreo = new javax.swing.JTextField();
+        txtTelefono = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel();
+        btnReproducir = new javax.swing.JButton();
+        btnDetener = new javax.swing.JButton();
+        btnAnterior = new javax.swing.JButton();
+        btnSiguiente = new javax.swing.JButton();
+        btnAgregar = new javax.swing.JButton();
+        btnModificar = new javax.swing.JButton();
+        btnEliminar = new javax.swing.JButton();
+        btnSalir = new javax.swing.JButton();
+        btnGuardarC = new javax.swing.JButton();
+        btnCargarAnimal = new javax.swing.JButton();
+        lbImg = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel2.setBackground(new java.awt.Color(219, 226, 254));
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED), "Persona registrada"));
+        jPanel2.setToolTipText("");
+        jPanel2.setName(""); // NOI18N
+
+        jLabel1.setText("Cedula:");
+
+        jLabel2.setText("Nombre:");
+
+        jLabel3.setText("Apellido");
+
+        jLabel4.setText("Correo");
+
+        jLabel5.setText("Telefono");
+
+        txtCedula.setEditable(false);
+
+        txtNombre.setEditable(false);
+
+        txtApellido.setEditable(false);
+
+        txtCorreo.setEditable(false);
+
+        txtTelefono.setEditable(false);
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5))
+                .addGap(26, 26, 26)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtCedula, javax.swing.GroupLayout.DEFAULT_SIZE, 183, Short.MAX_VALUE)
+                    .addComponent(txtNombre)
+                    .addComponent(txtApellido)
+                    .addComponent(txtCorreo)
+                    .addComponent(txtTelefono))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(txtCedula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtApellido, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(txtCorreo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(txtTelefono, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED))
+        );
+
+
+        jPanel1.setBackground(new java.awt.Color(230, 242, 254));
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED), "Menú de opciones"));
+        btnSalir.setText("Salir");
+        btnSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSalirActionPerformed(evt);
+            }
+        });
+
+        btnGuardarC.setText("Guardar cambios");
+        btnGuardarC.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGuardarCActionPerformed(evt);
+            }
+        });
+
+        btnCargarAnimal.setText("Ingresar Persona");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(94, 94, 94)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnAnterior, javax.swing.GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE)
+                    .addComponent(btnAgregar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnCargarAnimal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(20, 20, 20)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnGuardarC, javax.swing.GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+                    .addComponent(btnSalir, javax.swing.GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+                    .addComponent(btnReproducir, javax.swing.GroupLayout.DEFAULT_SIZE, 130, Short.MAX_VALUE)
+                    .addComponent(btnDetener, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnModificar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(btnSiguiente, javax.swing.GroupLayout.DEFAULT_SIZE, 124, Short.MAX_VALUE)
+                    .addComponent(btnEliminar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(90, 90, 90))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAnterior)
+                    .addComponent(btnSiguiente))
+                .addGap(3, 3, 3)
+                .addComponent(btnReproducir)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnDetener)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnAgregar)
+                    .addComponent(btnModificar)
+                    .addComponent(btnEliminar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnGuardarC)
+                    .addComponent(btnCargarAnimal))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+                .addComponent(btnSalir))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 8, Short.MAX_VALUE))))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(96, 96, 96)
+                .addComponent(lbImg, javax.swing.GroupLayout.PREFERRED_SIZE, 398, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(lbImg, javax.swing.GroupLayout.PREFERRED_SIZE, 390, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnSalirActionPerformed
+
+    private void btnGuardarCActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarCActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnGuardarCActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Interfaz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interfaz().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton btnAgregar;
+    public javax.swing.JButton btnAnterior;
+    public javax.swing.JButton btnCargarAnimal;
+    public javax.swing.JButton btnDetener;
+    public javax.swing.JButton btnEliminar;
+    public javax.swing.JButton btnGuardarC;
+    public javax.swing.JButton btnModificar;
+    public javax.swing.JButton btnReproducir;
+    public javax.swing.JButton btnSalir;
+    public javax.swing.JButton btnSiguiente;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    public javax.swing.JLabel lbImg;
+    public javax.swing.JTextField txtCedula;
+    public javax.swing.JTextField txtNombre;
+    public javax.swing.JTextField txtApellido;
+    public javax.swing.JTextField txtCorreo;
+    public javax.swing.JTextField txtTelefono;
+    // End of variables declaration//GEN-END:variables
 }
--- TallerInterfazBD/src/logica/Controlador.java
+++ TallerInterfazBD/src/logica/Controlador.java
@@ -17,7 +17,19 @@ public class Controlador implements ActionListener{
 
 
     public Controlador(Persona persona, Interfaz interfaz){
-
+        this.persona = persona;
+        this.interfaz = interfaz;
+        //Listeners de los botónes
+        this.interfaz.btnAnterior.addActionListener(this);
+        this.interfaz.btnSiguiente.addActionListener(this);
+        this.interfaz.btnReproducir.addActionListener(this);
+        this.interfaz.btnDetener.addActionListener(this);
+        this.interfaz.btnAgregar.addActionListener(this);
+        this.interfaz.btnModificar.addActionListener(this);
+        this.interfaz.btnEliminar.addActionListener(this);
+        this.interfaz.btnGuardarC.addActionListener(this);
+        this.interfaz.btnSalir.addActionListener(this);
+        this.interfaz.btnCargarAnimal.addActionListener(this);
 
     }
 
@@ -28,13 +40,34 @@ public class Controlador implements ActionListener{
     }
 
     public void mostrarVista(){
+        Persona an=new Persona(); //Instancio objeto animal
+        an=aPersona .get(posArr); //Asigno al objeto el animal de la posición escogida
+        this.interfaz.txtCedula.setText(an.getCedula());
+        this.interfaz.txtNombre.setText(an.getNombre());
+        this.interfaz.txtApellido.setText(an.getApellido());
+        this.interfaz.txtCorreo.setText(an.getCorreo());
+        this.interfaz.txtTelefono.setText(an.getTelefono());
         
     }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource()== this.interfaz.btnAgregar){//PUlSACIÓN DEL BOTÓN AGREGAR
+            this.interfaz.txtCedula.setText("");
+            this.interfaz.txtNombre.setText("");
+            this.interfaz.txtApellido.setText("");
+            this.interfaz.txtCorreo.setText("");
+            this.interfaz.txtTelefono.setText("");
+            this.interfaz.txtCedula.setEditable(true);//Seteo a editables las cajas de texto
+            this.interfaz.txtNombre.setEditable(true);
+            this.interfaz.txtApellido.setEditable(true);
+            this.interfaz.txtCorreo.setEditable(true);
+            this.interfaz.txtTelefono.setEditable(true);
+            this.interfaz.btnCargarAnimal.setVisible(true);//Muestro botón Adicionar animal 
+            this.interfaz.lbImg.setIcon(null);//Quito la imagen de la pantalla
         
+        }else{}
     }
 
 }
